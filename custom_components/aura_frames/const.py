44281716cd0590db5e322dc178ce507870091928
SparrowSX <161722489+SparrowSX@users.noreply.github.com>
2026-07-27 23:33:17 +0200

    Create const.py

---
+++ custom_components/aura_frames/const.py
@@ -0,0 +1,12 @@
+"""Constants for Aura Frames."""
+
+DOMAIN = "aura_frames"
+
+DEFAULT_SCAN_INTERVAL = 60
+
+CONF_USER_ID = "user_id"
+CONF_TOKEN = "token"
+CONF_CLIENT_ID = "client_id"
+CONF_FRAME_ID = "frame_id"
+
+API_BASE = "https://api.pushd.com/v5"
