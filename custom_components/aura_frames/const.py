1fc12cfe5d086261af2eab112ca57e896bb1da1b
SparrowSX <161722489+SparrowSX@users.noreply.github.com>
2026-07-27 23:42:16 +0200

    Update const.py

--- custom_components/aura_frames/const.py
+++ custom_components/aura_frames/const.py
@@ -1,12 +1,14 @@
 """Constants for Aura Frames."""
 
+from datetime import timedelta
+
 DOMAIN = "aura_frames"
 
-DEFAULT_SCAN_INTERVAL = 60
+API_URL = "https://api.pushd.com/v5"
+
+DEFAULT_SCAN_INTERVAL = timedelta(seconds=60)
 
 CONF_USER_ID = "user_id"
 CONF_TOKEN = "token"
 CONF_CLIENT_ID = "client_id"
 CONF_FRAME_ID = "frame_id"
-
-API_BASE = "https://api.pushd.com/v5"
